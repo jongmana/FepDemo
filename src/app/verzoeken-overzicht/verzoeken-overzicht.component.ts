50792e20f5cbe3ce88d4774688c4c449b7e509ce
Alex Jongman <alex.jongman@hu.nl>
2019-09-24 16:03:53 +0200

    small changes

--- src/app/verzoeken-overzicht/verzoeken-overzicht.component.ts
+++ src/app/verzoeken-overzicht/verzoeken-overzicht.component.ts
@@ -1,34 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Verzoek } from 'src/app/shared/models/verzoek';
-import { VerzoekenService } from '../core/services/verzoeken.service';
+import { Component, OnInit } from "@angular/core";
+import { Verzoek } from "src/app/shared/models/verzoek";
+import { VerzoekenService } from "../core/services/verzoeken.service";
 
 @Component({
-  selector: 'app-verzoeken-overzicht',
-  templateUrl: './verzoeken-overzicht.component.html',
-  styleUrls: ['./verzoeken-overzicht.component.css']
+  selector: "app-verzoeken-overzicht",
+  templateUrl: "./verzoeken-overzicht.component.html",
+  styleUrls: ["./verzoeken-overzicht.component.css"]
 })
 export class VerzoekenOverzichtComponent implements OnInit {
+  verzoeken: Array<Verzoek> = [];
 
-  verzoeken: Array<Verzoek> =  [];
+  constructor(private verzoekenService: VerzoekenService) {}
 
-  constructor(private verzoekenService: VerzoekenService) {
-    this.verzoeken = verzoekenService.getAllVerzoeken();
+  ngOnInit() {
+    this.verzoeken = this.verzoekenService.getAllVerzoeken();
 
     // simulatie dat er elders in de app een nieuw verzoek was ingediend.
     this.verzoekenService.addVerzoek(
       new Verzoek(
         321,
-        'Sinter',
-        'Klaas',
-        'sinter.klaas@student.hu.nl',
-        'TCIT-VKFEP',
-        'Front-End Programming',
-        'was vergeten me in te schrijven'
+        "Sinter",
+        "Klaas",
+        "sinter.klaas@student.hu.nl",
+        "TCIT-VKFEP",
+        "Front-End Programming",
+        "was vergeten me in te schrijven"
       )
     );
   }
-
-  ngOnInit() {
-  }
-
 }
