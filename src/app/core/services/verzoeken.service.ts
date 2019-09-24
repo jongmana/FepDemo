873bd4698b17f5d407be44b54f04a5b6db1471e7
Alex Jongman <alex.jongman@hu.nl>
2019-09-24 16:02:07 +0200

    met observer

--- src/app/core/services/verzoeken.service.ts
+++ src/app/core/services/verzoeken.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Verzoek } from "src/app/shared/models/verzoek";
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: "root"
 })
 export class VerzoekenService {
   private verzoeken: Array<Verzoek> = [];
+  verzoekenUpdate = new Subject<Verzoek[]>();
+
 
   constructor() {
     this.verzoeken.push(
@@ -30,6 +33,7 @@ export class VerzoekenService {
 
   addVerzoek(verzoek: Verzoek) {
     this.verzoeken.push(verzoek);
-    console.log(this.verzoeken);
+    // omdat er nu iets is veranderd moeten we anderen die zich op de updates hebben geabonneert informeren dat er iets is aangepast.
+    this.verzoekenUpdate.next(this.getAllVerzoeken()); // vuur een event af met daarin de nieuwe lijst van verzoeken.
   }
 }
--- src/app/verzoeken-overzicht/verzoeken-overzicht.component.ts
+++ src/app/verzoeken-overzicht/verzoeken-overzicht.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Verzoek } from 'src/app/shared/models/verzoek';
 import { VerzoekenService } from '../core/services/verzoeken.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-verzoeken-overzicht',
   templateUrl: './verzoeken-overzicht.component.html',
   styleUrls: ['./verzoeken-overzicht.component.css']
 })
-export class VerzoekenOverzichtComponent implements OnInit {
-
+export class VerzoekenOverzichtComponent implements OnInit, OnDestroy {
+  verzoekenAbo: Subscription;
   verzoeken: Array<Verzoek> =  [];
 
   constructor(private verzoekenService: VerzoekenService) {
-    this.verzoeken = verzoekenService.getAllVerzoeken();
 
     // simulatie dat er elders in de app een nieuw verzoek was ingediend.
     this.verzoekenService.addVerzoek(
@@ -26,9 +26,25 @@ export class VerzoekenOverzichtComponent implements OnInit {
         'was vergeten me in te schrijven'
       )
     );
+
   }
 
   ngOnInit() {
+    this.verzoeken = this.verzoekenService.getAllVerzoeken(); // initiele vulling van de list van verzoeken
+
+    this.verzoekenAbo = this.verzoekenService.verzoekenUpdate.subscribe(
+      (verzoeken: Verzoek[]) => {
+        this.verzoeken = verzoeken;
+      }
+    )
+
+  }
+
+  ngOnDestroy() {
+    // - vergeet niet de OnDestroy klasse bij de implements bovenaan toe te voegen.
+    // - omdat we nu een subscription hebben moeten we deze ook weer aan het einde van de lifecycle van deze klasse weer verwijderen;
+    // anders kun je vreemde bijwerkingen krijgen tijdens het runnen van de applicatie omdat de klasse maar blijft reageren op updates.
+    this.verzoekenAbo.unsubscribe();
   }
 
 }
