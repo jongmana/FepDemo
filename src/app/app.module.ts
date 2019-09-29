01eb16cafac3e71bf1d4c1ebeb83150604482d67
Alex-Jongman <alex.jongman@gmail.com>
2019-09-29 17:50:07 +0200

    Routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { VerzoekenOverzichtComponent } from './verzoeken-overzicht/verzoeken-overzicht.component';
 import { VerzoekFormulierComponent } from './verzoek-formulier/verzoek-formulier.component';
+import { MenuComponent } from './menu/menu.component';
 
+const appRoutes: Routes = [
+  { path: 'verzoek_indienen', component: VerzoekFormulierComponent },
+  { path: 'verzoeken_overzicht', component: VerzoekenOverzichtComponent },
+  { path: '**', component: VerzoekenOverzichtComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     VerzoekenOverzichtComponent,
-    VerzoekFormulierComponent
+    VerzoekFormulierComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/verzoek-formulier/verzoek-formulier.component.ts
+++ src/app/verzoek-formulier/verzoek-formulier.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Verzoek } from '../shared/models/verzoek';
 import { VerzoekenService } from '../core/services/verzoeken.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verzoek-formulier',
@@ -11,7 +12,9 @@ export class VerzoekFormulierComponent implements OnInit, OnDestroy {
 
   verzoekform = new Verzoek(0, '', '', '', '', '', ''); 
 
-  constructor(private verzoekenService: VerzoekenService) { }
+  constructor(
+    private verzoekenService: VerzoekenService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,6 +24,7 @@ export class VerzoekFormulierComponent implements OnInit, OnDestroy {
     // vandaar dat deze op dit moment ontbreekt; We gaan ervan uit dat alle velden netjes en goed worden gevuld.
 
     this.verzoekenService.addVerzoek(this.verzoekform);
+    this.router.navigate(['/verzoekenOverzicht']);
   }
 
   ngOnDestroy() {
